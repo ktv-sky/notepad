2373901025fa55511df8eaad07b9de4d96fe7ad5
ktv-sky <helicooper7@gmail.com>
2020-10-20 12:49:53 +0300

    Add authentication, login required

--- users/urls.py
+++ users/urls.py
@@ -6,15 +6,5 @@ from . import views
 app_name = 'users'
 urlpatterns = [
     path('', include('django.contrib.auth.urls')),
+    path('register/', views.register, name='register'),
 ]
-
-
-
-# users/ login/ [name='login']
-# users/ logout/ [name='logout']
-# users/ password_change/ [name='password_change']
-# users/ password_change/done/ [name='password_change_done']
-# users/ password_reset/ [name='password_reset']
-# users/ password_reset/done/ [name='password_reset_done']
-# users/ reset/<uidb64>/<token>/ [name='password_reset_confirm']
-# users/ reset/done/ [name='password_reset_complete']
